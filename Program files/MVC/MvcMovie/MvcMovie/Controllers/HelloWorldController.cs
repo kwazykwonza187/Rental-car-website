160fd602560365d49faed1a99d3a27cbbeb60de7
kwazykwonza187 <hiroshiransom@gmail.com>
2014-05-06 11:11:35 +0800

    Added files
    Added documents/programs related to the project.

---
+++ Program files/MVC/MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,32 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/ 
+
+        //public string Index()
+        //{
+         //   return "This is my <b>default</b> action...";
+        //}
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public ActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewBag.Message = "Hello " + name;
+            ViewBag.numTimes = numTimes;
+
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
