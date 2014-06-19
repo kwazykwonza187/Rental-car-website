3374181ffd76094a675b2741914a6cadbc65d20d
kwazykwonza187 <hiroshiransom@gmail.com>
2014-06-19 16:15:26 +0800

    Project created
    Created CRUD for RCW project. Just for the cars page.

---
+++ RCW/RCW/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RCW.MvcApplication" Language="C#" %>
---
+++ RCW/RCW/Models/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System;
+using System.Data.Entity;
+
+namespace RCW.Models
+{
+    public class Car
+    {
+        public int CarID { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public DateTime Release { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class CarDBContext : DbContext
+    {
+        public DbSet<Car> Movies { get; set; }
+    }
+}
